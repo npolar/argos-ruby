2801b50a933b73836ac1ddff788c673b66284379
cnrdh <conrad.helgeland@gmail.com>
2014-03-04 17:57:41 +0100

    argos-soap :)

---
+++ lib/argos/soap.rb
@@ -0,0 +1,325 @@
+module Argos
+  class Soap
+
+    # A "simple" Soap client for Argos-system satellite tracking webservice
+    # http://wanderingbarque.com/nonintersecting/2006/11/15/the-s-stands-for-simple/   
+    
+    # request [String] Soap:Envelope (XML request body)
+    # response [Savon::Response]
+    # operation [Savon::Operation]
+    # xml [String] (Extracted, inner) XML
+    attr_accessor :request, :response, :operation, :xml
+    
+    URI = "http://ws-argos.cls.fr/argosDws/services/DixService"
+    # Alternative: "http://ws-argos.clsamerica.com/argosDws/services/DixService"
+    
+    WSDL = "#{URI}?wsdl"       
+    
+    ARGOS_NS = "http://service.dataxmldistribution.argos.cls.fr/types"
+    SOAP_NS = "http://www.w3.org/2003/05/soap-envelope"
+    
+    NAMESPACES = {
+      "soap" => SOAP_NS,
+      "argos" => ARGOS_NS
+    }
+    
+    # Constructor
+    # soap = Argos::Soap.new({username: "argos-system-user", password: "argos-system-pw"})
+    def initialize(config={})
+      config.each do |k,v|
+        case k.to_sym
+        when :username
+          @username=v
+        when :username
+          @password=v
+        when :wsdl
+          @wsdl=v
+        when :programNumber
+          @programNumber = v
+        when :platformId
+          @platformId = v
+        when :nbDaysFromNow
+          @nbDaysFromNow = v.to_i
+        else
+          #raise ArgumentError, "Unkown config key: #{k}"
+        end
+      end
+    end
+
+    # Build baseRequest Hash
+    # The service requires programNumber or PlatformId, but if you do not provide any,
+    # this method will call the service (@see #programs) and get the current user's programs
+    # @return [Hash]
+    def baseRequest
+      # if override key is platformId... delete programNumber...
+      # if override key is period... delete nbDaysFromNow...
+      baseRequest = { username: _username, password: _password }
+      
+      # First choice (program or platform)
+      if @programNumber.nil? and @platformId.nil?
+        # Fetch all programs if neither is provided
+        baseRequest[:programNumber] = programs.map {|p|p.to_s}.join(",")
+      elsif @programNumber =~ /\d+/ and @platformId =~ /\d+/
+        raise "Cannot provide both programNumber and platformId"
+      elsif @programNumber =~ /\d+/ 
+        baseRequest[:programNumber] = @programNumber
+      elsif @platformId =~ /\d+/
+        baseRequest[:platformId] = @platformId
+      end
+      
+      # 2nd choice (time)
+      if @nbDaysFromNow.nil? and @period.nil?
+        # Default to 20 days of data (the maximum)
+        baseRequest[:nbDaysFromNow] = 20
+      elsif @nbDaysFromNow =~ /\d+/ and not @period.nil?
+        raise "Cannot provide both nbDaysFromNow and period"
+      elsif @nbDaysFromNow.to_s =~ /\d+/ 
+        baseRequest[:nbDaysFromNow] = @nbDaysFromNow.to_i
+      else
+        baseRequest[:period] = @period
+      end
+      
+      baseRequest = baseRequest.merge({ 
+        #<xs:element minOccurs="0" name="referenceDate" type="tns:referenceDateType"/>
+        #<xs:element minOccurs="0" name="locClass" type="xs:string"/>
+        #<xs:element minOccurs="0" name="geographicArea" type="xs:string"/>
+        #<xs:element minOccurs="0" name="compression" type="xs:int"/>
+        #<xs:element minOccurs="0" name="mostRecentPassages" type="xs:boolean"/>
+      })
+      baseRequest
+      
+    end
+        
+    # @return [Savon]
+    def client
+      @client ||= begin  
+        
+        tmpwsdl = "/tmp/argos-soap.wsdl"        
+        if File.exists?(tmpwsdl)
+          uri = tmpwsdl
+        else
+          uri = @wsdl||=WSDL
+        end
+        
+        client = ::Savon.new(uri)
+        if not File.exists?(tmpwsdl)
+          response = ::Net::HTTP.get_response(::URI.parse(WSDL))
+          File.open(tmpwsdl, "wb") { |file| file.write(response.body)}
+        end
+        client
+      end
+    end
+
+    # @return [String]
+    def getCsv
+      o = _operation(:getCsv)
+      o.body = { csvRequest: baseRequest.merge(
+        showHeader: true).merge(xmlRequest)
+      }
+      @response = o.call
+      @request = o.build
+      @text = _extract_escaped_xml("csvResponse").call(response)
+    end
+    
+    # @return [Hash]
+    def getKml
+      _call_xml_operation(:getKml, { kmlRequest: baseRequest.merge(xmlRequest)}, _extract_escaped_xml("kmlResponse"))
+    end
+    
+    def getPlatformList
+      _call_xml_operation(:getPlatformList, { platformListRequest:
+        # Cannot use #baseRequest here because that methods calls #programs which also calls #getPlatformList...
+        { username: _username, password: _password },
+      }, _extract_escaped_xml("platformListResponse"))
+    end
+    
+    # @return [Hash]
+    def getXml
+      _call_xml_operation(:getXml,
+        { xmlRequest: baseRequest.merge(xmlRequest)},
+        _extract_escaped_xml("xmlResponse"))
+    end
+    
+    # @return [Hash]
+    def getStreamXml
+      _call_xml_operation(:getStreamXml,
+        { streamXmlRequest: baseRequest.merge(xmlRequest)},
+        _extract_motm)
+    end 
+    
+    # @return [Hash]
+    def getXsd  
+      _call_xml_operation(:getXsd, { xsdRequest: {} }, _extract_escaped_xml("xsdResponse"))
+    end
+    
+    # @return [Hash]
+    # choice: programNumber | platformId | wmo*
+    # nbMaxObs
+    def getObsXml
+      _call_xml_operation(:getObsXml,
+        { observationRequest: baseRequest.merge(xmlRequest)},
+        _extract_escaped_xml("observationResponse"))
+    end
+    
+    # @return [Text]
+    # choice: programNumber | platformId | wmo*
+    # nbMaxObs
+    def getObsCsv
+      o = _operation(:getObsCsv)
+      o.body = { observationRequest: baseRequest.merge(xmlRequest)
+      }
+      @response = o.call
+      @request = o.build
+      @text = _extract_escaped_xml("observationResponse").call(response)
+    end
+    
+    # @return [Array]
+    def platforms
+      raise "@todo"
+    end
+    
+    # @return [Array]
+    def programs
+      getPlatformList["data"]["program"].map {|p| p["programNumber"].to_i }
+    end
+
+    # @return [String]
+    def raw
+      response.raw
+    end
+    
+    # @return [String]
+    def request
+      operation.build
+    end
+    
+    # @return [Hash] {"DixService":{"ports":{"DixServicePort":{"type":"http://schemas.xmlsoap.org/wsdl/soap12/","location":"http://ws-argos.cls.fr/argosDws/services/DixService"}}}}
+    def services
+      client.services
+    end
+    
+    # @return [String]
+    def text
+      @text||=""
+    end
+
+    # @return [Array] [:getCsv, :getStreamXml, :getKml, :getXml, :getXsd, :getPlatformList, :getObsCsv, :getObsXml] 
+    def operations
+      @response = client.operations(:DixService, :DixServicePort)
+    end
+    
+    def namespaces
+      NAMESPACES
+    end
+    
+    def xmlRequest
+      {
+        displayLocation: true,
+        displayDiagnostic: true,
+        displayMessage: true,
+        displayCollect: true,
+        displayRawData: true,
+        displaySensor: true,
+        #argDistrib: "",
+        displayImageLocation: true,
+        displayHexId: true  
+      }
+    end
+    
+    protected
+    
+    # Build and call @operation, set @response, @request, and @xml
+    # @raise on faults
+    # @return [Hash]
+    def _call_xml_operation(op_sym, body, extract=nil)
+      
+      @operation = _operation(op_sym)
+      @operation.body = body
+      @response = operation.call
+
+      # Check for http errors?
+
+      # Handle faults (before extracting data)
+      _envelope.xpath("soap:Body/soap:Fault", namespaces).each do | fault |
+        raise fault.to_s
+      end
+      
+      # Extract data
+      if extract.respond_to?(:call)
+        @xml = extract.call(response)
+      else
+        @xml = response.raw
+      end
+      
+      # Handle errors
+      ng = Nokogiri.XML(xml)
+      ng.xpath("/data/errors/error").each do | error |
+        raise error.to_s
+        # FIXME Custom exception so that it mighht be trapped on raw responses...
+      end
+      
+      # Validation
+      # Validation against XSD does not work: ["Element 'data': No matching global declaration available for the validation root."]
+      # schema = Nokogiri::XML::Schema(File.read("/tmp/argos-data.xsd"))
+      # v = schema.validate(ng)
+      
+      # Convert XML to Hash
+      nori = Nori.new
+      nori.parse(xml)
+    end
+    
+    # This is a shame, but who's to blame when there's multiple XML prologs (the second is escaped) and even mix of (declared) encodings (UTF-8 in soap envelope, ISO-8859-1 inside)
+    # Note: the inner data elements are non-namespaced (see da), so that recreating as proper XML would need to set xmlns=""
+    def _extract_escaped_xml(responseElement)
+      lambda {|response| CGI.unescapeHTML(response.raw.split("<#{responseElement} xmlns=\"http://service.dataxmldistribution.argos.cls.fr/types\"><return>")[1].split("</return>")[0])}
+    end
+    
+    # This is proof-of-concept quality code
+    # @return [String]
+    def _extract_motm
+      lambda {|response|
+        # Scan for MOTM signature --uuid:*
+        if response.raw =~ (/^(--[\w:-]+)--$/)
+          # Get the last message, which is -2 because of the trailing --
+          xml = response.raw.split($1)[-2].strip
+
+          # Get rid of HTTP headers
+          if xml =~ /\r\n\r\n[<]/
+            xml = xml.split(/\r\n\r\n/)[-1]
+          end
+          
+        else
+          raise "Cannot parse MOTM"
+        end
+        }
+    end
+    
+    # @return [Nokogiri:*]
+    def _envelope
+      ng = Nokogiri.XML(response.raw).xpath("/soap:Envelope", namespaces)
+      if not ng.any?
+        # Again, this is a shame...
+        envstr = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">'
+        extracted = envstr + response.raw.split(envstr)[1].split("</soap:Envelope>")[0] + "</soap:Envelope>"
+        ng = Nokogiri.XML(extracted).xpath("/soap:Envelope", namespaces)
+      end
+      ng
+    end
+        
+    # @return [Savon::Operation]
+    def _operation(operation_name)
+      client.operation(:DixService, :DixServicePort, operation_name)
+    end
+    
+    # @return [String]
+    def _username
+      @username||=ENV["ARGOS_SOAP_USERNAME"]
+    end
+    
+    # @return [String]
+    def _password
+      @password||=ENV["ARGOS_SOAP_PASSWORD"]
+    end
+    
+  end  
+end
---
+++ lib/argos/soap_command.rb
@@ -0,0 +1,202 @@
+require "optparse"  
+require "net/http"
+require "csv"
+
+module Argos
+  # 
+  class SoapCommand
+     
+    CMD = "argos-soap"
+    
+    PARAM = { format: :json,
+      wsdl: Argos::Soap::WSDL,
+      handle_exception: lambda {
+        |e, soap| #noop
+        }
+      }
+    
+    def self.run(argv=ARGV)
+      begin
+        cmd = new(argv)
+        cmd.run
+        exit(true)
+      rescue => e
+        puts e
+        puts e.backtrace.join("\n")
+        exit(false)
+      end
+    end
+    
+    attr_accessor :log, :param, :result, :soap
+  
+    def initialize(argv=ARGV, param = PARAM)
+      @param = param
+
+      option_parser = OptionParser.new(argv) do |opts|
+      
+        opts.version = Argos::VERSION
+      
+        opts.banner = "#{CMD} operation [options]\n"
+
+        opts.on("--debug", "Debug (alias for --log-level=0") do
+          @param[:level] = Logger::DEBUG
+        end
+
+        opts.on("--format=format", "-f=format", "{ json | xml | text | raw }") do |format|
+          @param[:format] = format.to_sym
+        end
+        
+        opts.on("--operation=operation", "-o=operation", "{ #{operations.join(" | ")} }") do |operation|
+          @param[:operation] = operation.to_sym
+          # Change format (from JSON) for XML and text-operations
+          if [:getKml, :getObsXml, :getStreamXml, :getXml, :getXsd].include? param[:operation]
+            @param[:format] = :xml
+          elsif [:getCsv].include? param[:operation]
+            @param[:format] = :text
+          end          
+        end
+        
+        opts.on("--log-level=level", "Log level") do |level|
+          @param[:level] = level.to_i
+        end
+        
+        opts.on("--nbDaysFromNow=days", "Set number of days") do |nbDaysFromNow|
+          @param[:nbDaysFromNow] = nbDaysFromNow
+        end
+        
+        opts.on("--operations", "List operations") do
+          @param[:operation] = :operations
+        end
+                
+        opts.on("--platforms", "List platforms") do
+          @param[:operation] = :platforms
+        end
+        
+        opts.on("--programs", "List programs") do
+          @param[:operation] = :programs
+        end
+        
+        opts.on("--programNumber=program1,program2", "Set programNumber(s)") do |programNumber|
+          @param[:programNumber] = programNumber
+        end
+        
+        opts.on("--platformId=platform1,platform2", "Set platformId(s)") do |platformId|
+          @param[:platformId] = platformId
+        end
+
+        opts.on("--services", "Services (from WSDL)") do
+          @param[:operation] = :services
+        end
+
+        opts.on("--username=username", "Set username") do |username|
+          if not username.nil?
+            @param[:username] = username
+          end
+        end
+        
+        opts.on("--password=password", "Set passord") do |password|
+          if not password.nil?
+            @param[:password] = password
+          end
+        end
+
+      end
+      option_parser.parse!
+      
+      if operation.nil?
+        puts option_parser.help
+        exit(false)
+      end
+      
+      @log = Logger.new(STDERR)
+      @log.level = param[:level]
+    end
+    
+    def debug?
+      @param[:level] == Logger::DEBUG
+    end
+    
+    def run
+        @soap = Argos::Soap.new(param)
+        
+        @result = nil
+        
+        case param[:operation]
+        when :getCsv
+          @result = soap.getCsv 
+        when :getPlatformList
+          @result = soap.getPlatformList
+        when :getKml
+          @result = soap.getKml
+        when :getObsCsv
+          @result = soap.getObsCsv
+        when :getObsXml
+          @result = soap.getObsXml
+        when :getStreamXml
+          @result = soap.getStreamXml
+        when :getXml
+          @result = soap.getXml
+        when :getXsd
+          @result = soap.getXsd
+        when :operations
+          @result = soap.operations
+        when :platforms
+          @result = soap.platforms
+        when :programs
+          @result = soap.programs
+        when :services
+          @result = soap.services
+        else
+          raise ArgumentError, "Unspported operation: #{param[:operation]}"  
+        end
+        output
+
+    end
+    
+    protected
+    
+    def handle_exception(e)
+      param[:handle_exception].call(e, soap)
+    end
+    
+    def output
+      if debug?
+        log.debug "#{CMD} param: #{param.to_json}"
+        log.debug "$ curl -XPOST #{Argos::Soap::URI} -d'#{soap.request}' -H \"Content-Type: application/soap+xml\""
+      end
+      if result.is_a? Savon::Response
+        @result = result.raw
+      end
+      
+      case format.to_sym
+      
+      when :ruby
+        puts result
+      when :json
+        puts result.to_json
+      when :text
+        puts soap.text
+      when :raw
+        puts soap.raw
+      when :xml
+        puts soap.xml
+      else
+        raise ArgumentError, "Unknown format: #{format}"
+      end
+      
+    end
+    
+    def format
+      param[:format]
+    end
+    
+    def operation
+      param[:operation]
+    end
+    
+    def operations
+      [:getCsv, :getStreamXml, :getKml, :getXml, :getXsd, :getPlatformList, :getObsCsv, :getObsXml] 
+    end
+    
+  end
+end
