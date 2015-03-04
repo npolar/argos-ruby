3871c688f1c0ac2836fb151f42e002cc14cfc406
cnrdh <cnrdh@users.noreply.github.com>
2015-03-04 15:33:10 +0100

    Added hex string and hex array support

--- lib/argos/kiwisat303.rb
+++ lib/argos/kiwisat303.rb
@@ -5,14 +5,17 @@ module Argos
   # The sensor data consists of a 24 bit / 3 byte sequence (#binary_sensor_data)
   #
   # Binary sensor data, from left:
-  # * [00..02] = message_type (where 0 is data, 7 is diagnostics)
+  # * [00..02] = message_type (0 is a data message, 2 and 7 ("10" and "111") are engineering diagnostics 
   # * [03..06] = voltage
   # * [07..11] = transmissions (count)
   # * [12..20] = temperature
   # * [21..23] = day type (transmission scheduling)
   class Kiwisat303
     
+    @sensor_data_format = "integer"
+    
     attr_reader :sensor_data
+    attr_writer :sensor_data_format
     
     # Fixed-length (defaults to 8 bits) binary from integer-forced number
     # @return [String]
@@ -35,20 +38,39 @@ module Argos
       binary_sensor_data[21..23].to_i(2)
     end
     
-    # Set sensor data array of numbers (integers or strings containing integers or 2-digit hex numbers)
-    # @param [Array<String|Integer>]
+    # Set sensor data, as one of:
+    # * array of numbers (integers)
+    # * array of strings containing integers or 2-digit hex numbers (*)
+    # * hex string
+    # 
+    # If you set sensor data as array of hex strings, you must also set sensor_data_format to "hex"
+    #
+    # @param [Array<String|Integer> | String ] 
     def sensor_data=(sensor_data)
-      # @todo How about real Hex members
+      
+      # Hex string?
+      if sensor_data.is_a? String
+        bytes = (sensor_data.size/2)
+        sensor_data = sensor_data.scan(/[0-9a-f]{2}/i)
+        if not sensor_data.size == bytes
+          raise ArgumentError, "Invalid hex in sensor data string"
+        end
+        @sensor_data_format = "hex"
+      end
+      
+      # At least 3 members?
       if not sensor_data.is_a? Array or sensor_data.size < 3
         raise ArgumentError, "Sensor data should be an Array of at least 3 scalar elements"
       end
       
       # Hex sensor data?
       if sensor_data.all? {|sd| sd.to_s =~ /^[0-9a-f]{2}$/i }
-        sensor_data = sensor_data.map {|sd| sd.to_s.to_i(16)}
+        if "hex" == @sensor_data_format
+          sensor_data = sensor_data.map {|sd| sd.to_s.to_i(16)}
+        end
       end
       
-      # String-bearing integer sensor data?
+      # Convert integers-in-string to integers
       sensor_data = sensor_data.map {|d|
         if d.is_a? String and d.to_s =~ /^[0-9]{1,3}$/i and d.to_i <= 255
           d = d.to_i
@@ -56,6 +78,7 @@ module Argos
         d        
       }
       
+      # Validate, all members should now be integers between 0 and 255
       if sensor_data.any? {|sd| sd.to_i != sd or sd < 0 or sd > 255 }
         raise ArgumentError, "Invalid sensor data: Not array of integers between 0-255"
       end
--- spec/argos/kiwisat303_spec.rb
+++ spec/argos/kiwisat303_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper.rb'
 module Argos
   describe Kiwisat303 do
 
-    context "sensor data = [27, 150, 201]" do
+    context "sensor data = (integer/string array) [\"27\", 150, 201]" do
       
       before do
         @kiwisat = Argos::Kiwisat303.new
-        @kiwisat.sensor_data = ["27", "150", 201]
+        @kiwisat.sensor_data = ["27", 150, 201]
         @expected = { message_type: 0,
           voltage: 3.536,
           transmissions: 3200,
@@ -57,7 +57,66 @@ module Argos
           expect(@kiwisat.voltage).to eq(@expected[:voltage])
         end
       end
-    
+      
     end
+    
+    context "sensor data = (hex string) \"464F96[..]\"" do
+      
+      before do
+        @kiwisat = Argos::Kiwisat303.new
+        @kiwisat.sensor_data = "464F96C20F98CD8BE2C02FE5D56BC9DFE6A0D836876F2796443FF8"
+        @expected = { message_type: 2,
+          voltage: 2.896,
+          transmissions: 512,
+          temperature: 44.1858
+        }
+      end
+      
+      describe "#data" do
+        it do
+          expect(@kiwisat.data).to eq(@expected)
+        end 
+      end
+      
+      describe "#sensor_data" do
+        it do
+          expect(@kiwisat.sensor_data).to eq([70, 79, 150, 194, 15, 152, 205, 139, 226, 192, 47, 229, 213, 107, 201, 223, 230, 160, 216, 54, 135, 111, 39, 150, 68, 63, 248])
+        end
+      end
+      
+      it "invalid hex should raise ArgumentError" do    
+        expect { @kiwisat.sensor_data = "46FG" }.to raise_error(ArgumentError)
+      end
+      
+      # Hmm 11-bytes array ["19", "208", "25", "91", "53", "104", "30", "241", "194", "131", "76"] is still message_type 0 ?
+      # 113910  11660 77.86706  18.11291           A 2014-12-22T15:44:31Z  NN  NA 19|208|25|91|53|104|30|241|194|131|76          344            1833              64  771     NA Arctic fox
+      
+    end
+    
+    
+    context "sensor data = (hex array) [\"19\",\"04\", \"A1\"]" do
+      
+      before do
+        @kiwisat = Argos::Kiwisat303.new
+        @kiwisat.sensor_data_format="hex"
+        @kiwisat.sensor_data = ["19","04", "A1"]
+        @expected = {:message_type=>0, :voltage=>3.472, :transmissions=>2048, :temperature=>6.1535}
+      end
+      
+      describe "#data" do
+        it do
+          expect(@kiwisat.data).to eq(@expected)
+        end 
+      end
+      
+      describe "#sensor_data = [25, 4, 161]" do
+        it do
+          expect(@kiwisat.sensor_data).to eq([25, 4, 161])
+        end
+      end
+      
+    end
+    
+  
   end
 end
