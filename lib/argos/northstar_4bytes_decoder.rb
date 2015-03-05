e80006a6699998d152553362aea9332598c457af
cnrdh <cnrdh@users.noreply.github.com>
2015-03-05 13:17:06 +0100

    Now decodes all 4 message types

--- lib/argos/northstar_4bytes_decoder.rb
+++ lib/argos/northstar_4bytes_decoder.rb
@@ -1,23 +1,106 @@
 module Argos
   
-  # NorthStar 4-bytes sensor data extractor
+  # NorthStar 4-bytes sensor data decoder
   #
-  # Extracts sensor data from 32 bit / 4 byte sequence (#binary_sensor_data)
+  # [Documentation](https://raw.githubusercontent.com/npolar/argos-ruby/master/documents/northstarst.com/message-formats-4-byte-sensor-data.txt)
   #
-  # https://github.com/npolar/api.npolar.no/wiki/Tracking-API-Vendor-North-Star
-  class NorthStar4Bytes
+  # Each message consists of a 4-byte data stream, which contains three sections; a message ID, the sensor data, and the message checksum.
+  # Thus the 4-byte data stream has the following format;
+  #
+  # Message Byte	Description
+  # 0	Message_ID
+  # 1	Sensor_Byte_0
+  # 2	Sensor_Byte_1
+  # 3	Message_Checksum
+  #
+  # The following messages are currently used;
+  #
+  # Message_ID	Sensor_Byte_0	Sensor_Byte_1
+  # 00	Current Season	Activity Count
+  # 01	Tx Count (MSB)	Tx Count (LSB)
+  # 02	Battery Voltage	Temperature (C)
+  # 03	System Week	System Hour
+  #
+  # The message ID is a unique single byte number identifying the sensor data which is contained in the message.
+  # For example message 00 will always contain the Current Season (Sensor_Byte_0) and the Activity Count (Sensor_Byte_1).
+  # As new sensors become available new unique message IDs will be assigned.
+
+  class NorthStar4BytesDecoder
     
     include SensorData
     
     # @return [Hash]
     def data
       data = { message_type: message_type }
-      if message_type == 2
+      if message_type == 0
+        data[:current_season] = current_season
+        data[:activity_count] = activity_count
+      elsif message_type == 1
+        data[:transmissions] = transmissions    
+      elsif message_type == 2
+        data[:voltage] = voltage
         data[:temperature] = temperature
+      elsif message_type == 3
+        data[:system_weeks] = system_weeks
+        data[:system_hours] = system_hours
+        data[:run_time] = run_time
       end
       data
     end
+
+    def message_type
+      sensor_data[0]
+    end
+    alias :message_id :message_type
     
+    # (1) Current Season
+    # This is the integer value of the data received.
+    def current_season
+      sensor_data[1]
+    end
+    
+    # (2) Activity Count
+    # This is the integer value of the data received.
+    def activity_count
+      sensor_data[2]
+    end
+    
+    # (3) Transmission Counter
+    # Combine the hexadecimal value of the two bytes received and convert to decimal notation.
+    def transmissions
+      "#{hex(sensor_data[1])}#{hex(sensor_data[2])}".to_i(16)
+    end
+        
+    # (4) Battery Voltage (volts)
+    # Convert the received data (Bat_volt_byte) using the following formula;
+    # Battery Voltage = 2 x (Bat_volt_byte) x (0.01)
+    #
+    # For example, if the Bat_volt_byte received is B7H (i.e. 183 decimal) then the actual battery voltage is
+    #
+    # Battery Voltage = 2 X (183) X (0.01) (volts)
+    # or,
+    # Battery Voltage = 3.66 (volts).
+    def voltage
+      voltage_byte = sensor_data[1]
+      2 * voltage_byte * 0.01
+    end
+    
+    # (5) Temperature (Centigrade)
+    # This is the signed integer value of the received data in whole degrees. Signed integers use the most significant bit to indicate whether the number is positive (in binary notation bit 7 = 0) or negative (bit 7 = 1).
+    # Positive numbers are converted directly to integers. Negative numbers require that a two's complement conversion of the received data be performed to get the signed integer result. In order to perform a two's complement conversion,
+    # first complement the number and then add a one to the result, remembering to append the negative sign to the number. For example, signed integer ECH is converted to a "signed" number as follows,
+    #
+    # ECH = 1110 1100 (binary)
+    # Complement of ECH = 0001 0011
+    # Two's complement of ECH = (-) (0001 0011) + (0000 0001)
+    # = (-) (0001 0100) (binary)
+    # = (-) 14H	(hexadecimal)
+    # = (-) 20 (decimal)
+    #
+    # On the other hand, if the temperature data is 14H then since the sign bit (bit 7) is positive the temperature is positive,
+    #
+    # Temperature = 14H = 0001 0100 (binary) = 20 (decimal)
+    # Temperature = (+) 20 centigrade.
     def temperature
       if sensor_data[2] <= 127
         sensor_data[2]
@@ -26,8 +109,21 @@ module Argos
       end
     end
     
-    def message_type
-      sensor_data[0]
+    # (7) System Week Count
+    # This is the integer value of the data received.	Remember a "System Week" is 167 "System Hours". Thus a "System Week" is about 605368.32 standard seconds which is exceeds a standard week by about 568 seconds.
+    def system_weeks
+      sensor_data[1]
+    end
+    
+    # (6) System Hour Count
+    # This is the integer value of the data received. Remember a "System Hour" equals about 3624.96 standard seconds.
+    def system_hours
+      sensor_data[2]
+    end
+    
+    # Run Time = 2 X (605368.32) + 162 X (3624.96) [sec]
+    def run_time
+      system_weeks*605368.32 + system_hours*3624.96
     end
     
     protected
--- spec/argos/northstar_4bytes_decoder_spec.rb
+++ spec/argos/northstar_4bytes_decoder_spec.rb
@@ -1,29 +1,53 @@
 require 'spec_helper.rb'
 
 module Argos
-  describe NorthStar4Bytes do
+  describe NorthStar4BytesDecoder do
     
     before do
-      @n = NorthStar4Bytes.new
-      @n.sensor_data = ["02","176","46","156"]
-      @expected = [2, 176, 46, 156]
+      @n = NorthStar4BytesDecoder.new
+      @n.sensor_data = ["02","176","46","156"] # message type 2 (set in the first byte)
+      @expected_sensor_data = [2, 176, 46, 156]
     end
     
-    it "sensor_data is forced to arpray of 10-base integers" do
-      expect(@n.sensor_data).to eq(@expected)
+    it "#sensor_data= forces data to array of 10-base integers" do
+      expect(@n.sensor_data).to eq(@expected_sensor_data)
     end
     
-    it "#data returns a Hash with :message_type and :temperature" do
-      expect(@n.data).to eq({:message_type=>2, :temperature=>46})
+    describe "#data" do
+      context "message type 0" do
+        it "#data is a Hash with current_season and activity_count" do
+          @n.sensor_data = [00,01,248,249]
+          expect(@n.data).to eq({:message_type=>0, current_season: 1, activity_count: 248})
+        end
+      end
+      context "message type 1" do
+        it "#data is a Hash with transmissions (count)" do
+          @n.sensor_data = [1,3,245,255] # 01 03 F5
+          # "03F5" == 1013
+          expect(@n.data).to eq({:message_type=>1, transmissions: 1013})
+        end
+      end
+      context "message type 2" do
+        # message type 2 is set in the root before block
+        it "#data is Hash with voltage and temperature" do
+          expect(@n.data).to eq({:message_type=>2, :voltage=>3.52, :temperature=>46})
+        end
+      end
+      context "message type 3" do
+        it "#data is Hash with system_weeks, system_hours, and run_time (seconds)" do
+          @n.sensor_data = [03, 02, 162, 163] # from documentation
+          expect(@n.data).to eq({:message_type=>3, :system_weeks=>2, :system_hours=>162, :run_time => 1797980.16}) # Run Time = 1,797,980.16 [sec]
+        end
+      end
     end
+
     
-    context "#temperature" do
+    describe "#temperature (Celsius)" do
       
       context "[+]" do
-        it "positive temperature is equal to the 3rd byte of 4" do
-          expect(@n.temperature).to eq(@expected[2])
+        it "positive temperature is equal to sensor byte 3 (of 4)" do
+          expect(@n.temperature).to eq(@expected_sensor_data[2])
         end
-        
       end
       
       context "[-]" do
@@ -33,10 +57,16 @@ module Argos
         it "negative temperature is the complement of the binary number plus 1" do
           expect(@n.temperature).to eq(-20)
         end
-        
-        
       end
       
+    end
+    
+    describe "#voltage (V)" do
+      it do
+        expect(@n.voltage).to eq(3.52)
+        @n.sensor_data = [2,183,0,0] # One more test, from the documentation (183 => 3.66)
+        expect(@n.voltage).to eq(3.66)
+      end
     end
     
   end
