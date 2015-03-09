0c7ca52d8eddf5d5fca351da4ef96ab0a6c38784
cnrdh <cnrdh@users.noreply.github.com>
2015-03-09 15:37:41 +0100

    More verbose test output

--- spec/argos/northstar_4bytes_decoder_spec.rb
+++ spec/argos/northstar_4bytes_decoder_spec.rb
@@ -5,8 +5,8 @@ module Argos
     
     before do
       @n = NorthStar4BytesDecoder.new
-      @n.sensor_data = ["02","176","46","156"] # message type 2 (set in the first byte)
-      @expected_sensor_data = [2, 176, 46, 156]
+      @n.sensor_data = ["02","174","20","184"] #["02","176","46","156"] # message type 2 (set in the first byte)
+      @expected_sensor_data = [2, 174, 20, 184]
     end
     
     it "#sensor_data= forces data to array of 10-base integers" do
@@ -14,34 +14,37 @@ module Argos
     end
     
     describe "#data" do
-      context "message type 0" do
-        it "#data is a Hash with current_season and activity_count" do
+      context "message type 0: #{[00,01,248,249].to_json}" do
+        exp = {:message_type=>0, current_season: 1, activity_count: 248}
+        it "#data is a Hash with current_season and activity_count: #{exp.to_json}" do
           @n.sensor_data = [00,01,248,249]
-          expect(@n.data).to eq({:message_type=>0, current_season: 1, activity_count: 248})
+          expect(@n.data).to eq(exp)
         end
       end
-      context "message type 1" do
-        it "#data is a Hash with transmissions (count)" do
+      context "message type 1: #{[1,3,245,255].to_json}" do
+        exp = {:message_type=>1, transmissions: 1013}
+        it "#data is a Hash with transmissions (count): #{exp.to_json}" do
           @n.sensor_data = [1,3,245,255] # 01 03 F5
           # "03F5" == 1013
-          expect(@n.data).to eq({:message_type=>1, transmissions: 1013})
+          expect(@n.data).to eq(exp)
         end
       end
-      context "message type 2" do
+      context "message type 2: #{[02,174,20,184].to_json}" do
+        exp = {:message_type=>2, :voltage=>3.48, :temperature=>20}
         # message type 2 is set in the root before block
-        it "#data is Hash with voltage and temperature" do
-          expect(@n.data).to eq({:message_type=>2, :voltage=>3.52, :temperature=>46})
+        it "#data is Hash with voltage and temperature: #{exp.to_json}" do
+          expect(@n.data).to eq(exp)
         end
       end
-      context "message type 3" do
-        it "#data is Hash with system_weeks, system_hours, and run_time (seconds)" do
+      context "message type 3: #{[03, 02, 162, 163].to_json}" do
+        exp = {:message_type=>3, :system_weeks=>2, :system_hours=>162, :run_time => 1797980.16}
+        it "#data is Hash with system_weeks, system_hours, and run_time (seconds): #{exp.to_json}" do
           @n.sensor_data = [03, 02, 162, 163] # from documentation
-          expect(@n.data).to eq({:message_type=>3, :system_weeks=>2, :system_hours=>162, :run_time => 1797980.16}) # Run Time = 1,797,980.16 [sec]
+          expect(@n.data).to eq(exp) # Run Time = 1,797,980.16 [sec]
         end
       end
     end
 
-    
     describe "#temperature (Celsius)" do
       
       context "[+]" do
@@ -62,8 +65,8 @@ module Argos
     end
     
     describe "#voltage (V)" do
-      it do
-        expect(@n.voltage).to eq(3.52)
+      it "183 = 3.66V" do
+        expect(@n.voltage).to eq(3.48)
         @n.sensor_data = [2,183,0,0] # One more test, from the documentation (183 => 3.66)
         expect(@n.voltage).to eq(3.66)
       end
