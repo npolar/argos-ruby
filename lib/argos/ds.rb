b2afef7b5e6b441b1274ce5dfcbcf675a8afdbe9
cnrdh <cnrdh@users.noreply.github.com>
2018-03-06 12:39:30 +0100

    Syntax cleanup; removed inline errors/warnings

--- lib/argos/ds.rb
+++ lib/argos/ds.rb
@@ -9,9 +9,9 @@ module Argos
   #
   # @author Espen Egeland
   # @author Conrad Helgeland
-  # 
+  #
   class Ds < Array
-    include Argos::Ascii  
+    include Argos::Ascii
 
     attr_writer :log, :filename, :bundle
 
@@ -31,7 +31,7 @@ module Argos
     def filter?
       not @filter.nil?
     end
-    
+
     def filter=filter
       if filter.respond_to? :call
         @filter = filter
@@ -50,7 +50,7 @@ module Argos
 
     # Parses Argos DS file and returns Argos::Ds -> Array
     #
-    # The parser loops all messages (stored in @messages), before #unfold 
+    # The parser loops all messages (stored in @messages), before #unfold
     # creates a sorted Array of measurements
     #
     #@param filename [String] Filename of Argos DS file
@@ -64,20 +64,20 @@ module Argos
       if filename.nil?
         filename = @filename
       end
-      
+
 
       filename = File.realpath(filename)
       @filename = filename
       if filename.nil? or not File.exists? filename
         raise ArgumentError, "Missing ARGOS DS file: \"#{filename}\""
       end
-      @sha1 = Digest::SHA1.file(filename).hexdigest 
+      @sha1 = Digest::SHA1.file(filename).hexdigest
 
       contact = []
       file = File.open(filename)
       @filesize = file.size
       @updated = file.mtime.utc
-      
+
       log.debug "Parsing ARGOS DS file #{filename} source:#{sha1} (#{filesize} bytes)"
       if filter?
         log.debug "Using filter: #{@filtername.nil? ? filter : @filtername }"
@@ -96,7 +96,7 @@ module Argos
         #if (c+1) % 1000 == 0
         #  log.debug "Line: #{c+1}"
         #end
-        
+
         if line =~ START_REGEX
 
           @valid = true
@@ -104,31 +104,31 @@ module Argos
           if contact.any?
             item = parse_message(contact)
 
-            if self.class.valid_item? item  
-              
+            if self.class.valid_item? item
+
               if not filter? or filter.call(item)
                 @messages << item
               end
-              
+
             else
               raise "Argos DS message #{filename}:#{c} lacks required program and/or platform"
             end
           end
-  
+
           contact = [line]
 
-        else 
+        else
           # 2010-12-14 15:11:34  1         00           37           01           52
           if contact.any? and line != ""
             contact << line
           end
         end
       end
-  
+
       if false == @valid
         #log.debug file.read
         message = "Cannot parse file: #{filename}"
-        raise ArgumentError, message 
+        raise ArgumentError, message
       end
 
       last = parse_message(contact)
@@ -139,8 +139,8 @@ module Argos
           @messages << last
         end
       end
-      
-      log.debug "Parsed #{@messages.size} Argos DS messages into #{self.class.name} Array"  
+
+      log.debug "Parsed #{@messages.size} Argos DS messages into #{self.class.name} Array"
       @segments = @messages.size
       unfold.each do |d|
         self << d
@@ -155,7 +155,7 @@ module Argos
       self.sort_by! {|ds| ds[:measured]}
       self
     end
-  
+
     # Pare one DS segment
     def parse_message(contact)
       header = contact[0]
@@ -163,13 +163,13 @@ module Argos
       items = process_item_body(body)
       combine_header_with_transmission(items, header)
     end
-  
+
     def type
       "ds"
     end
-  
+
     # @param [String] header
-    # Header is is a space-separated string containing 
+    # Header is is a space-separated string containing
     #   [0] Program number
     #   [1] Platform number
     #   [2] Number of lines of data per satellite pass
@@ -189,7 +189,7 @@ module Argos
     #  ["09660", "10788", "5", "3", "H", "2", "1992-04-06", "22:12:16", "78.248", "15.505", "0.000", "401649604"]
     #  ["09660", "10788", "2", "3", "D"]
     # http://www.argos-system.org/files/pmedia/public/r363_9_argos_users_manual-v1.5.pdf page 42
-    # 
+    #
     # Warning, the parser does not support this header format from 1989 [AUO89.DAT]
     # 19890800-19891000: ["09660", "14653", "10", "41", "14", "1", "-.42155E+1", "00", "112", "17DD"]
     def combine_header_with_transmission(measurements, header)
@@ -209,7 +209,7 @@ module Argos
       if header[8] != nil && valid_float?(header[8])
         latitude = header[8].to_f
       end
-  
+
       if header[9] != nil && valid_float?(header[9])
         longitude = header[9].to_f
         if (180..360).include? longitude
@@ -221,15 +221,15 @@ module Argos
       if not altitude.nil?
          altitude = altitude.to_f*1000
       end
-        
+
       if positioned.nil? and measurements.nil?
         warn << "missing-time"
       end
-        
+
       if latitude.nil? or longitude.nil?
         #warn << "missing-position"
       else
-      
+
         unless latitude.between?(-90, 90) and longitude.between?(-180, 180)
           errors << "invalid-position"
         end
@@ -239,8 +239,8 @@ module Argos
         errors << "invalid-lc"
       end
 
-      # Satellites 
-      #  ["A", "B", "K", "L", "M", "N", "P", "R"] 
+      # Satellites
+      #  ["A", "B", "K", "L", "M", "N", "P", "R"]
 
       document = { program:  header[0].to_i,
         platform: header[1].to_i,
@@ -251,7 +251,7 @@ module Argos
         positioned: positioned,
         latitude: latitude,
         longitude: longitude,
-        altitude: altitude,        
+        altitude: altitude,
         measurements: measurements,
         headers: header.size
       }
@@ -261,10 +261,10 @@ module Argos
       if errors.any?
         document[:errors]=errors
       end
-      
+
       document
     end
-  
+
     # Merge position and all other top-level DS fields with each measurement line
     # (containing sensor data)
     # The 3 lines below will unfold to *2* documents, each with
@@ -281,16 +281,16 @@ module Argos
       messages.select {|ds|
         ds.key?(:measurements) and not ds[:measurements].nil?
       }.each do |ds|
-        
+
         ds[:measurements].each_with_index do |measurement,cardinality|
           # Cardinality 0 will filter out repeated positions due to higher sensor sampling rate...
           unfolded << merge(ds,measurement, cardinality)
         end
       end
- 
+
       unfolded = unfolded.sort_by {|ds|
         if not ds[:measured].nil?
-          DateTime.parse(ds[:measured]) 
+          DateTime.parse(ds[:measured])
         elsif not ds[:positioned].nil?
           DateTime.parse(ds[:positioned])
         else
@@ -306,9 +306,9 @@ module Argos
     # Merges a DS header hash into each measurement
     # @return [Array] Measurements with header and static metadata merged in
     def merge(ds, measurement, cardinality)
-      
-      m = ds.select {|k,v| k != :measurements and k != :errors and k != :warn }     
-      m = m.merge(measurement)          
+
+      m = ds.select {|k,v| k != :measurements and k != :errors and k != :warn }
+      m = m.merge(measurement)
       m = m.merge ({ technology: "argos",
         type: type,
         cardinality: cardinality
@@ -316,23 +316,23 @@ module Argos
         #source: sha1
       })
 
-      if not ds[:errors].nil? and ds[:errors].any?
-        m[:errors] = ds[:errors].clone
-      end
-
-      if not ds[:warn].nil? and ds[:warn].any?
-        m[:warn] = ds[:warn].clone
-      end
-
-      if not m[:sensor_data].nil? and m[:sensor_data].size != ds[:sensors]
-        if m[:warn].nil?
-          m[:warn] = []
-        end
-        m[:warn] << "sensors-count-mismatch"
-      end
+      # if not ds[:errors].nil? and ds[:errors].any?
+      #   m[:errors] = ds[:errors].clone
+      # end
+      #
+      # if not ds[:warn].nil? and ds[:warn].any?
+      #   m[:warn] = ds[:warn].clone
+      # end
+      #
+      # if not m[:sensor_data].nil? and m[:sensor_data].size != ds[:sensors]
+      #   if m[:warn].nil?
+      #     m[:warn] = []
+      #   end
+      #   m[:warn] << "sensors-count-mismatch"
+      # end
 
       # Create id as SHA1 hash of measurement minus stuff that may vary (like filename)
-      # 
+      #
       # Possible improvement for is to base id on a static list of keys
       # :program,
       # :platform,
@@ -356,12 +356,12 @@ module Argos
       idbase.delete :errors
       idbase.delete :file
       idbase.delete :warn
-      
+
       id = Digest::SHA1.hexdigest(idbase.to_json)
 
       #m[:parser] = Argos.library_version
       m[:id] = id
-      
+
       #m[:bundle] = bundle
       m
     end
@@ -371,8 +371,8 @@ module Argos
       @transmission_arr = []
       @transmission_arr = recursive_transmission_parse(body_arr)
     end
-  
-  
+
+
     # @param [Array] body_arr
     # @return  [Aray]
     def recursive_transmission_parse(body_arr)
@@ -380,7 +380,7 @@ module Argos
         return
       end
       @buf =@buf + " " + body_arr[0]
-  
+
       if body_arr[1] =~ /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/ or body_arr[1]==nil
         @transmission_arr << transmission_package(@buf)
         @buf=""
@@ -388,12 +388,12 @@ module Argos
       recursive_transmission_parse(body_arr[1,body_arr.length])
       @transmission_arr
     end
-  
+
     def transmission_package(data)
       transmission_time = data[/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/,1]
       transmission_time = convert_datetime(transmission_time)
 
-      identical = data.split(" ")[2].to_i    
+      identical = data.split(" ")[2].to_i
       data = data.strip[23,data.length]
 
       if not data.nil?
@@ -416,16 +416,16 @@ module Argos
     def source
       @sha1
     end
-    
+
     protected
 
     # "1999-04-02 01:28:54"
     def convert_datetime(datetime)
-    
+
     #AUO89.DAT/home/ch/github.com/argos-ruby/lib/ds.rb:143:in `parse': can't convert nil into String (TypeError)
     #/home/ch/github.com/api.npolar.no/seed/tracking/argos/19890800-19891000
-    #AUO89.DAT/home/ch/github.com/argos-ruby/lib/ds.rb:149:in `parse': invalid date (ArgumentError)  
-      begin  
+    #AUO89.DAT/home/ch/github.com/argos-ruby/lib/ds.rb:149:in `parse': invalid date (ArgumentError)
+      begin
         datetime = ::DateTime.parse(datetime).iso8601.to_s
         datetime['+00:00'] = "Z"
         datetime
@@ -442,14 +442,14 @@ module Argos
     end
 
     # Argos format until 1991
-    # 
+    #
     #09660   6 09691  2 14286 89 042 17 18 05 1   3 G     0.000 401650000        0VDI
     #09660      2     59.891   10.629                           401649651        0VDJ
     #09660 14286 17 14 26  1  -.72543E+1           00                            0VDK
     #09660 14286 17 16 52  1  -.72410E+1           00                            0VDL
     #09660 14286 17 19 18  2  -.72376E+1           00                            0VDM
     #09660 14286 17 21 44  3  -.72376E+1           00                            0VDN
-    
+
     # Header: 09660[program]   6[lines] ????? 2 ????? 89[year] 042[day?] 17 18 05[time?] 1   3[?] G[?]    0.000 \d{9}[f] \d\w{3}[ident]
     def parse_legacy(file)
       raise "Legacy DS file parser: not implemented"
