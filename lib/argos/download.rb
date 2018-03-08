d352087dba54764a43c8ad3dbae0a91102ecbfa3
cnrdh <cnrdh@users.noreply.github.com>
2018-03-08 09:21:55 +0100

    Fixes #6

--- lib/argos/download.rb
+++ lib/argos/download.rb
@@ -5,7 +5,7 @@ require "digest"
 require "nokogiri"
 
 module Argos
-  
+
   # Download utility class to take the strain out of downloading and
   # archiving Argos XML and CSV for multiple users, programs and platforms.
   #
@@ -19,37 +19,37 @@ module Argos
 
 
   class Download
-    
+
     def self.download(username, password, archive, log, days=20)
-      
+
       log.debug "Starting download of Argos XML to #{archive}"
-      
+
       soap = Argos::Soap.new({username: username, password: password})
       soap.log = log
-      
+
       programs = soap.programs
       log.debug "Programs for #{username}: #{programs.to_json}"
-      year = DateTime.now.year
-  
+      #year_now = DateTime.now.year
+
       soap.getPlatformList["data"]["program"].each do |program|
         programNumber = program["programNumber"]
         soap.programNumber = programNumber
-      
+
         platforms = soap.platforms
-        
+
         # inactive (no last collect)
         inactive = program["platform"]
-        
+
         active = program["platform"].select {|platform|
           platform.key? "lastCollectDate" and platform.key? "lastLocationDate"}.select {|platform|
-          
+
           lastCollectDate = DateTime.parse(platform["lastCollectDate"])
           lastLocationDate = DateTime.parse(platform["lastLocationDate"])
-          
+
           twentydays = DateTime.parse((Date.today-20).to_s)
-          
+
           (lastCollectDate > twentydays or lastLocationDate > twentydays)
-          
+
         }
         # inactive = platforms where last collect date is missing or > 20 days
         inactive = program["platform"] - active
@@ -61,65 +61,67 @@ module Argos
           log.debug "Inactive [#{n+1}/#{inactive.size}]: #{i.reject{|k,v| k =~ /location/i }.to_json}"
         end
         active.each_with_index do | platform, idx |
-          
-      
+
+
           platformId = platform["platformId"]
           soap.platformId = platformId
-          
+
           log.debug "About to download program: #{programNumber}, platform: #{platformId} [#{idx+1}/#{active.size}], lastCollectDate: #{platform["lastCollectDate"]}"
-        
+
           20.downto(1) do |daysago|
-            
+
             date = Date.today-daysago
-            
+            year = date.year # Fixes https://github.com/npolar/argos-ruby/issues/6
             destination = "#{archive}/#{year}/program-#{programNumber}/platform-#{platformId}"
-          
+            
             begin
               soap.period = {startDate: "#{date}T00:00:00Z", endDate: "#{date}T23:59:59.999Z"}
               soap.getXml
-              
+
               FileUtils.mkdir_p(destination)
               filename = destination+"/argos-#{date}-platform-#{platformId}.xml"
-              
+
               if File.exists? filename
                 existing_sha1 = Digest::SHA1.file(filename).hexdigest
                 existing_errors = Argos::Soap.new.validate(File.read(filename))
                 if existing_errors.any?
                   log.error "Validation error for existing data #{filename} (#{File.size(filename)} bytes): #{existing_errors.uniq.to_json}"
-                end 
+                end
                 #log.debug "Keeping existing file #{filename} from #{File.mtime(filename)}, fresh data is identical"
               end
-              
+
               new_xml_ng = Nokogiri::XML(soap.xml, nil, nil, Nokogiri::XML::ParseOptions::NOBLANKS | Nokogiri::XML::ParseOptions::NOCDATA | Nokogiri::XML::ParseOptions::STRICT)
               new_xml = new_xml_ng.canonicalize
               new_sha1 = Digest::SHA1.hexdigest(new_xml)
-              
+              updating = false
+
               if existing_sha1.nil? or existing_errors.any? or existing_sha1 != new_sha1
-              
+
                 if errors = Argos::Soap.new.validate(new_xml_ng).any?
                   raise "Failed XML schema validation"
                 else
                   File.open(filename, "wb") { |file| file.write(new_xml)}
                   log.debug "Validated and saved new data: #{filename}"
+                  updating = true
                 end
-                
+
               end
-              log.debug "Day -#{daysago}: #{date}: #{new_xml_ng.xpath("//message").size} message(s) (program #{programNumber}, platform #{platformId})"
+              log.debug "Day -#{daysago}: #{date} [#{ updating == false ? 'validated and kept existing file' : 'updating'}]: #{new_xml_ng.xpath("//message").size} message(s) (program #{programNumber}, platform #{platformId})"
             rescue Argos::NodataException
               # noop
               log.debug "Day -#{daysago}: #{date}: No data for (program #{programNumber}, platform #{platformId})"
             rescue => e
               log.error e
             end
-            
+
           end
-          
+
           log.debug "Completed download of #{platformId}"
-          
+
         end
         log.debug "Completed download for #{username}"
       end
     end
-    
+
   end
 end
